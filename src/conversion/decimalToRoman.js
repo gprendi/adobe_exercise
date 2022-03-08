5edca2887ad5f7c3980a5d3b9a90bc75ac479835
Gerald Prendi <gerald.prendi@usi.ch>
2022-03-08 10:42:18 +0100

    Conversion function, tests for decimalToRoman pass

--- src/conversion/decimalToRoman.js
+++ src/conversion/decimalToRoman.js
@@ -1,5 +1,32 @@
-function decimalToRoman() {
-    return "";
+function decimalToRoman (num) {
+  function toRoman (number) {
+    let roman = ''
+
+    const num = [1, 4, 5, 9, 10, 40, 50, 90, 100]
+    const sym = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C']
+
+    // start from the last one, and work backwards
+    let i = 8
+    while (number > 0) {
+      let div = Math.floor(number / num[i])
+      number = number % num[i]
+      while (div--) {
+        roman += sym[i]
+      }
+      i--
+    }
+
+    return roman
+  }
+
+  if (num === undefined) throw new Error('Should be called with a parameter')
+  if (typeof num !== 'number') throw new Error('Only numbers can be converted')
+
+  // only positive numbers can be converted
+  // imposing a limit on maximum value
+  if (num < 1 || num > 255) throw new Error('Only positive numbers between 1 and 255 can be converted')
+
+  return toRoman(num)
 }
 
-module.exports.decimalToRoman = decimalToRoman;
+module.exports.decimalToRoman = decimalToRoman
